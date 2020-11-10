e667c6758d1efed0fe11b5c1af6bcd5059757e90
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-10 19:45:08 -0300

    building movies model

---
+++ Films/Home/Model/Movies.swift
@@ -0,0 +1,40 @@
+//
+//  Movies.swift
+//  Films
+//
+//  Created by Ezequiel Parada Beltran on 10/11/2020.
+//
+
+import Foundation
+
+struct Movies {
+    let listOfMovies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case listOfMovies = "results"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let popularity: Double
+    let movieID: Int
+    let voteCount: Int
+    let originalTitle: String
+    let voteAverage: Double
+    let sinposis: String
+    let releaseDate: String
+    let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case popularity
+        case movieId = "id"
+        case voteCount = "vote_count"
+        case originalTitle = "original_title"
+        case voteAverage = "vote_average"
+        case sinposis = "overview"
+        case releaseDate = "release_date"
+        case image = "poster_path"
+    }
+}
---
+++ NetworkManager/ManagerConnections.swift
@@ -0,0 +1,21 @@
+//
+//  ManagerConnections.swift
+//  Films
+//
+//  Created by Ezequiel Parada Beltran on 10/11/2020.
+//
+
+import Foundation
+import RxSwift
+
+class ManagerConnections {
+    
+    
+    func getPopularMovies() {
+        
+    }
+    
+    func getDetailMovies() -> Observable<[Movie]>{
+        
+    }
+}
