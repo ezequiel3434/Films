6f43c7d1e17d92c922a6e14ce11224d4611e5938
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-09 21:49:16 -0300

    building constants file

---
+++ Films/Resources/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Films
+//
+//  Created by Ezequiel Parada Beltran on 09/11/2020.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "44ce905d7875e2e9702161996fa09a52"
+    
+    struct URL {
+        static let main = "https://api.themoviedb.org/3/"
+        static let images = "https://image.tmdb.org/t/p/w200"
+
+    }
+    
+    struct EndPoints {
+        static let urlListPopularMobies = "movie/popular"
+        static let detailMovie = "movie/"
+    }
+}
