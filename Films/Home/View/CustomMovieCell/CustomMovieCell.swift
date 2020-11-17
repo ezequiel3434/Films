14777bcf1612b132cf3ad81b72cd1e5f5cf66419
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-16 21:33:51 -0300

    building CustomMovieCell and Connecting to code

---
+++ Films/Home/View/CustomMovieCell/CustomMovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomMovieCell.swift
+//  Films
+//
+//  Created by Ezequiel Parada Beltran on 16/11/2020.
+//
+
+import UIKit
+
+class CustomMovieCell: UITableViewCell {
+
+    @IBOutlet weak var imageMovie: UIImageView!
+    @IBOutlet weak var titleMovie: UILabel!
+    @IBOutlet weak var descriptionMovie: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Films/Home/View/HomeView.swift
+++ Films/Home/View/HomeView.swift
@@ -19,9 +19,15 @@ class HomeView: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureTableView()
         viewModel.bind(view: self, router: router)
         getData()
+        
+    }
+    
+    private func configureTableView(){
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(UINib(nibName: "CustomMovieCell", bundle: nil), forCellReuseIdentifier: "CustomMovieCell")
     }
 
     private func getData(){
@@ -58,10 +64,15 @@ extension HomeView: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-        cell.textLabel?.text = movies[indexPath.row].originalTitle
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CustomMovieCell.self)) as! CustomMovieCell
+        cell.titleMovie.text = movies[indexPath.row].title
+        cell.descriptionMovie.text = movies[indexPath.row].sinposis
+        
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
      
 }
